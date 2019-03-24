7c71b1203d16bc4b3cf90d15f824da9b81075ebf
Andre Bonfatti <andrebnf@gmail.com>
2019-03-24 14:58:21 -0300

    config: installed dependencies

--- mix.exs
+++ mix.exs
@@ -13,9 +13,6 @@ defmodule TagTag.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {TagTag.Application, []},
@@ -23,22 +20,21 @@ defmodule TagTag.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.2"},
+      {:phoenix, "~> 1.4.2", override: true},
+      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
+      {:plug_cowboy, "~> 2.0"},
+      {:plug, "~> 1.7"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_html, "~> 2.13", github: "phoenixframework/phoenix_html", override: true},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:cowboy, "~> 2.0"}
     ]
   end
 end
