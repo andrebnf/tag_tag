783937c988c12f401fb2a04ebf1dd9416008aa80
Andre Bonfatti <andrebnf@gmail.com>
2019-03-25 21:11:19 -0300

    feat: working live view

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,5 +12,5 @@ import css from "../css/app.css"
 import "phoenix_html"
 import LiveSocket from "phoenix_live_view"
 
-const liveSocket = new LiveSocket("/live")
+let liveSocket = new LiveSocket("/live")
 liveSocket.connect()
--- lib/tag_tag_web/controllers/page_controller.ex
+++ lib/tag_tag_web/controllers/page_controller.ex
@@ -1,7 +1,8 @@
 defmodule TagTagWeb.PageController do
   use TagTagWeb, :controller
+  alias Phoenix.LiveView
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    LiveView.Controller.live_render(conn, MyAppWeb.UserView, session: %{})
   end
 end
